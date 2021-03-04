c240971367efd9b447aee98bef00d8365040ea93
jocneto <jocneto20@yahoo.com.br>
2021-03-04 23:11:55 +0000

    tentativa 1

---
+++ src/main/java/br/com/impacta/models/carro.java
@@ -0,0 +1,27 @@
+package br.com.impacta.models;
+
+public class carro{
+
+  private String cor;
+  private int ano;
+  private double valor;
+
+public String getcor(){
+  return cor;
+}
+public void setcor(String cor){
+  this.cor = cor;
+}
+public int getano(){
+  return ano;
+}
+public void setano(int ano){
+  this ano;
+}
+public int getvalor(){
+  return valor;
+}
+public void setvalor(double valor){
+  this valor;
+  }
+  
